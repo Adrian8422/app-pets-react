06c25d9cf2c5e3c444006dbc86cb3bed16fa4020
Adrian Villegas <manbassman1996@gmail.com>
2022-08-17 16:18:00 -0300

    changes-in-suspense

--- src/pages/me-reports/index.tsx
+++ src/pages/me-reports/index.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { MeReportsComp } from "components/me-reports";
 import { useReportesDelUser, useMeLatLng } from "hooks/atom";
-import { LoadingComp } from "components/loading";
 import css from "./meReports.css";
 function MeReportsPage() {
-  LoadingComp;
   return (
     <div className={css.root}>
       <MeReportsComp />
